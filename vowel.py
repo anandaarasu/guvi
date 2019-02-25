7bbdc79db96ef51cda92870958f419ae3cf8596a
anandaarasu <45783805+anandaarasu@users.noreply.github.com>
2019-02-25 13:35:35 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 n = input(" Enter the Character : ")
 
-if(ch == 'a' or  'e' or 'i' or ch 'o' or  'u' or  'A'
-       or 'E' or 'I' or  'O' or 'U'):
+if(ch == 'a' or ch== 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch== 'A'
+       orch == 'E' or ch =='I' or ch == 'O' or ch =='U'):
 print("Enter The Character ", ch, "is a Vowel")
 else:
 print("Enter The Character ", ch, "is a Consonant")
